28c36ff0d70a13c2e5709083b077aafa95275685
Yury Senna <yurysenasilva@gmail.com>
2025-06-12 21:34:00 -0300

    teste cell

---
+++ tabela.js
@@ -0,0 +1,60 @@
+function salvarTroca() {
+    const km = document.getElementById('kmRodados').value;
+    const data = document.getElementById('dataTroca').value;
+    const oleo = document.getElementById('txt-oleo').value;
+    const veiculo = document.getElementById('veiculo').value;
+
+    if (!km || !data) {
+      alert('Preencha todos os campos!');
+      return;
+    }
+
+    
+    let trocas = JSON.parse(localStorage.getItem('trocasOleo')) || [];
+
+    
+    trocas.push({ km, data, oleo, veiculo });
+    console.log(veiculo)
+    
+
+    localStorage.setItem('trocasOleo', JSON.stringify(trocas));
+
+    alert('Troca salva com sucesso!');
+  }
+
+  function mostrarTabela() {
+    const trocas = JSON.parse(localStorage.getItem('trocasOleo')) || [];
+    const corpo = document.getElementById('corpoTabela');
+    corpo.innerHTML = '';
+
+    trocas.forEach((troca) => {
+      corpo.innerHTML += `
+        <tr>
+          <td>${troca.data}</td>
+          <td>${troca.veiculo}</td>
+          <td>${troca.oleo}</td>
+          <td>${troca.km}</td>
+          
+        </tr> 
+      `;
+    });
+
+    document.getElementById('formulario').style.display = 'none';
+    document.getElementById('tabelaContainer').style.display = 'block';
+  }
+
+  function voltarParaFormulario() {
+  document.getElementById('formulario').style.display = 'flex'; 
+  document.getElementById('tabelaContainer').style.display = 'none';
+
+  
+  document.getElementById('kmRodados').value = '';
+  document.getElementById('txt-oleo').value = '';
+  document.getElementById('dataTroca').value = '';
+}
+
+
+
+
+
+
